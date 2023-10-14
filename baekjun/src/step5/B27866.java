c31b6aa6dda1cda5cee872c65b4bc2e6c0c9530e
hee-k <juullyy001@gmail.com>
2023-10-14 21:03:27 +0900

    231014

---
+++ baekjun/src/step5/B27866.java
@@ -0,0 +1,13 @@
+package step5;
+
+import java.util.Scanner;
+
+public class B27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        int n = sc.nextInt();
+
+        System.out.println(st.charAt(n-1));
+    }
+}
