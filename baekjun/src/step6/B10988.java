421bbdedd7a1181dae310d86476549e0bee32d94
hee-k <juullyy001@gmail.com>
2023-10-29 20:02:27 +0900

    231029

---
+++ baekjun/src/step6/B10988.java
@@ -0,0 +1,17 @@
+package step6;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B10988 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder(br.readLine());
+
+        bw.write(((sb.toString().equals(sb.reverse().toString())) ? 1 : 0) + "\n");
+        bw.flush(); bw.close(); br.close();
+    }
+}
