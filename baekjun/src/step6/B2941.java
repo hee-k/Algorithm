798ae91930d120b108c721c062ea23fb99101831
hee-k <juullyy001@gmail.com>
2023-11-01 21:35:52 +0900

    231101

--- baekjun/src/step6/B2941.java
+++ baekjun/src/step6/B2941.java
@@ -1,7 +1,16 @@
 package step6;
 
+import java.util.Scanner;
+
 public class B2941 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String str[] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String st = sc.nextLine();
+        for(int i = 0; i < str.length; i++) {
+            if(st.contains(str[i]))
+                st = st.replace(str[i], "!");
+        }
+        System.out.println(st.length());
     }
 }
