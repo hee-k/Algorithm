35057f8669b3b06eea1823a98f4fd3f58e3a8047
hee-k <juullyy001@gmail.com>
2023-11-08 23:13:42 +0900

    231108

---
+++ baekjun/src/step9/B1978.java
@@ -0,0 +1,21 @@
+package step9;
+
+import java.util.Scanner;
+
+public class B1978 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num;
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            num = sc.nextInt();
+            for(int j = 2; j <= num; j++) {
+                if(j == num) count++;
+                if(num % j == 0) break;
+            }
+        }
+        System.out.println(count);
+    }
+}
