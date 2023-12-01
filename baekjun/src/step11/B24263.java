35799d4b828b9468f782c77d3d4ad4a4627ee8eb
hee-k <juullyy001@gmail.com>
2023-12-01 22:51:29 +0900

    231201

---
+++ baekjun/src/step11/B24263.java
@@ -0,0 +1,16 @@
+package step11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); //입력 크기 입력받음 => 이것이 수행 홧수
+        br.close();
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
