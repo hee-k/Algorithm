2f45ec42c8a1ce1da7310f9e5cf036935a2d50a7
hee-k <juullyy001@gmail.com>
2023-12-28 23:37:45 +0900

    231228

---
+++ baekjun/src/step11/B24265.java
@@ -0,0 +1,18 @@
+package step11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+interface B24265 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println((n * (n - 1)) / 2);
+        System.out.println(2);
+    }
+}
