9ab93f961e803534fde79003513ad2753038117f
hee-k <juullyy001@gmail.com>
2024-01-01 23:27:41 +0900

    240101

---
+++ baekjun/src/step12/B24266.java
@@ -0,0 +1,17 @@
+package step12;
+
+import java.io.*;
+
+public class B24266 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+        br.close();
+
+        bw.write(n*n*n + "\n" + 3);
+        bw.flush();
+        bw.close();
+    }
+}
