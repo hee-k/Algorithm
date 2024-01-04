0c3b1f7a9f6395f72561d272dc11a3894cc3b7da
hee-k <juullyy001@gmail.com>
2024-01-04 22:14:38 +0900

    240104

--- codingTest/src/day2/B11720.java
+++ codingTest/src/day2/B11720.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class B11720 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+
+        int n = sc.nextInt();
+
         String sNum = sc.next();
+        // Stirng -> char
         char[] cNum = sNum.toCharArray();
 
         int sum = 0;
-        for (int i=0; i<cNum.length; i++) {
+        for (int i = 0; i < cNum.length; i++) {
+            // 문자 -> 숫자 변환(아스키코드)
             sum += cNum[i] - '0';
         }
         System.out.println(sum);
