0b44b87f6b64b27aa706869f90433550222669e6
hee-k <juullyy001@gmail.com>
2024-01-06 23:04:23 +0900

    240106

---
+++ codingTest/src/day3/B1940.java
@@ -0,0 +1,37 @@
+package day3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B1940 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int m = Integer.parseInt(bf.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+
+        int count = 0;
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            if (a[i] + a[j] < m) i++;
+            else if (a[i] + a[j] > m) j--;
+            else {
+                count++;
+                i++;
+                j--;
+            }
+        }
+        System.out.println(count);
+        bf.close();
+    }
+}
