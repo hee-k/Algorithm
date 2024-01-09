9971c8092c8f573a11881a1ce63e2d7ac6beda9b
hee-k <juullyy001@gmail.com>
2024-01-09 18:46:31 +0900

    240109

---
+++ codingTest/src/day3/B1874.java
@@ -0,0 +1,46 @@
+package day3;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class B1874 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
+        int num = 1;
+        boolean result = true;
+
+        for (int i = 0; i < a.length; i++) {
+            int seq = a[i];
+            if (seq >= num) {
+                // push
+                while (seq >= num) {
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            } else {
+                int m = stack.pop();
+                if (m > seq) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                } else {
+                    bf.append("-\n");
+                }
+            }
+        }
+        if (result) {
+            System.out.println(bf.toString());
+        }
+    }
+}
