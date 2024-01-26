2b1be862271904378116715e878673e63b803b0a
hee-k <juullyy001@gmail.com>
2024-01-26 12:23:32 +0900

    240126

---
+++ codingTest/src/day8/B2343.java
@@ -0,0 +1,38 @@
+package day8;
+
+import java.util.Scanner;
+public class B2343 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] a = new int[n];
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            if (start < a[i])
+                start = a[i];
+            end = end + a[i];
+        }
+        while (start <= end) {
+            int middle = (start + end) / 2;
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (sum + a[i] > middle) {
+                    count++;
+                    sum = 0;
+                }
+                sum = sum + a[i];
+            }
+            if (sum != 0)
+                count++;
+            if (count > m)
+                start = middle + 1;
+            else
+                end = middle - 1;
+        }
+        System.out.println(start);
+    }
+}
