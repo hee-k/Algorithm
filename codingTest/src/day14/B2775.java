56fbb0281dda1c16b3661a54c191ef680c9a0b2c
hee-k <juullyy001@gmail.com>
2024-02-24 22:59:59 +0900

    240224

---
+++ codingTest/src/day14/B2775.java
@@ -0,0 +1,27 @@
+package day14;
+
+import java.util.*;
+public class B2775 {
+    static int t, n, k;
+    static int[][] d;
+    public static void main(String[] args) throws Exception {
+        d = new int[15][15];
+        for (int i = 0; i < 15; i++) {
+            d[i][1] = 1;
+            d[0][i] = i;
+        }
+        for (int i = 1; i < 15; i++) {
+            for (int j = 2; j < 15; j++) {
+                d[i][j] = d[i][j-1] + d[i-1][j];
+            }
+        }
+        Scanner sc = new Scanner(System.in);
+        t = sc.nextInt();
+        for(int i=0; i<t; i++)
+        {
+            k = sc.nextInt();
+            n = sc.nextInt();
+            System.out.println(d[k][n]);
+        }
+    }
+}
