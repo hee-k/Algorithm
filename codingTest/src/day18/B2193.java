3dede1a14c166bd29de16cc0ad13c528322d2f2c
hee-k <juullyy001@gmail.com>
2024-03-13 01:20:37 +0900

    240313

---
+++ codingTest/src/day18/B2193.java
@@ -0,0 +1,18 @@
+package day18;
+
+import java.util.Scanner;
+public class B2193 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[][] d = new long[n+1][2];
+        d[1][1] = 1;
+        d[1][0] = 0;
+
+        for(int i=2; i<=n; i++){
+            d[i][0] = d[i-1][1] + d[i-1][0];
+            d[i][1] = d[i-1][0];
+        }
+        System.out.println(d[n][0] + d[n][1]);
+    }
+}
