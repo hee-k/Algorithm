8be5dd978e272a69793230c20f00592210d1d82a
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2025-09-22 18:46:54 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 224 ms, Memory: 18324 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int arr[] = new int[26];
+        Arrays.fill(arr, -1);
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(arr[c - 97] != -1) continue;;
+            arr[c - 97] = i;
+        }
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
