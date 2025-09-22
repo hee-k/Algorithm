107fb04cefb57671332bae7831b55189516002d8
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2025-09-22 19:28:13 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,20 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        int arr[] = new int[26];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[26];
         Arrays.fill(arr, -1);
 
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(arr[c - 97] != -1) continue;;
-            arr[c - 97] = i;
+        String word = br.readLine();
+
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (arr[idx] == -1) {
+                arr[idx] = i;
+            }
         }
-        for(int i : arr) {
-            System.out.print(i + " ");
+
+        for (int i = 0; i < 26; i++) {
+            sb.append(arr[i]);
+            if (i < 25) {
+                sb.append(' ');
+            }
         }
+
+        System.out.println(sb.toString());
     }
 }
