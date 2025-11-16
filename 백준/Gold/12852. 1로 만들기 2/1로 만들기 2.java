f9b4d87d4b0626a174e9912d4801653fc8a3c546
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2025-11-16 14:52:50 +0900

    [Gold V] Title: 1로 만들기 2, Time: 140 ms, Memory: 19764 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -9,36 +9,35 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n + 1];
-        int[] bf = new int[n + 1];
 
         for (int i = 2; i <= n; i++) {
             arr[i] = arr[i - 1] + 1;
-            bf[i] = i - 1;
 
             if (i % 2 == 0) {
-                int count = arr[i / 2] + 1;
-                if (count < arr[i]) {
-                    arr[i] = count;
-                    bf[i] = i / 2;
-                }
+                arr[i] = Math.min(arr[i], arr[i / 2] + 1);
             }
 
             if (i % 3 == 0) {
-                int count = arr[i / 3] + 1;
-                if (count < arr[i]) {
-                    arr[i] = count;
-                    bf[i] = i / 3;
-                }
+                arr[i] = Math.min(arr[i], arr[i / 3] + 1);
             }
         }
 
         System.out.println(arr[n]);
 
-        // 최솟값 만든 경로 역추적
-        int current = n;
-        while (current != 0) {
-            System.out.print(current + " ");
-            current = bf[current];
+        printPath(n, arr);
+    }
+
+    static void printPath(int n, int[] arr) {
+        System.out.print(n + " ");
+
+        if (n == 1) return;
+
+        if (n % 3 == 0 && arr[n] == arr[n / 3] + 1) {
+            printPath(n / 3, arr);
+        } else if (n % 2 == 0 && arr[n] == arr[n / 2] + 1) {
+            printPath(n / 2, arr);
+        } else {
+            printPath(n - 1, arr);
         }
     }
 }
