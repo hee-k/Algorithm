f313a5a6a020db9ff26b342cce4e388a3eff3530
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2025-11-26 23:16:18 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 516 ms, Memory: 61084 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static int[] parent;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        visited = new boolean[n + 1];
+        parent = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph.get(x).add(y);
+            graph.get(y).add(x);
+        }
+
+        bfs(1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= n; i++) {
+            sb.append(parent[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int idx = queue.poll();
+
+            for (int next : graph.get(idx)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    parent[next] = idx;
+                    queue.add(next);
+                }
+            }
+        }
+    }
+}
