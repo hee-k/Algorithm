bb87a1a57c2ec5df18517de9668d0ec8f521b292
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2026-01-04 22:26:57 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] basket = new int[n + 1];
+        for (int idx = 1; idx <= n; idx++) {
+            basket[idx] = idx;
+        }
+
+        for (int a = 0; a < m; a++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            int left = i;
+            int right = j;
+            while (left < right) {
+                int temp = basket[left];
+                basket[left] = basket[right];
+                basket[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int idx = 1; idx <= n; idx++) {
+            sb.append(basket[idx]).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
