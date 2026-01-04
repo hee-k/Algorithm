17f63500e7c84b3b328a119e5c620da0d4df274a
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2026-01-05 07:41:05 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -6,27 +6,21 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        String s = br.readLine();
 
         int[] arr = new int[26];
         Arrays.fill(arr, -1);
 
-        String word = br.readLine();
-
-        for (int i = 0; i < word.length(); i++) {
-            int idx = word.charAt(i) - 'a';
-            if (arr[idx] == -1) {
-                arr[idx] = i;
-            }
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            if (arr[idx] == - 1) arr[idx] = i;
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 26; i++) {
-            sb.append(arr[i]);
-            if (i < 25) {
-                sb.append(' ');
-            }
+            sb.append(arr[i]).append(" ");
         }
 
-        System.out.println(sb.toString());
+        System.out.println(sb.toString().trim());
     }
 }
