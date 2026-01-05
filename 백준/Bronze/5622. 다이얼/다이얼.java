b42cb7735b54a0760ef9bc8c9c1ab738a8a79bc3
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2026-01-05 20:18:20 +0900

    [Bronze II] Title: 다이얼, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int[] time = {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
+
+        int sum = 0;;
+        for (int i = 0; i < s.length(); i++) {
+            sum += time[s.charAt(i) - 'A'];
+        }
+
+        System.out.println(sum);
+    }
+}
