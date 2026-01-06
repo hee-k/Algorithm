e06a975f4a86ddd60b8ebf52db827f21930a9f37
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2026-01-07 07:36:23 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] arr = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max = 0;
+        int row = 0,  col = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.println((row + 1) + " " + (col + 1));
+    }
+}
