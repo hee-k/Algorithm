767d724d71d9789872b55e243d745afc2fd5b1d2
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2026-01-07 08:01:50 +0900

    [Bronze I] Title: 세로읽기, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] arr = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String word = br.readLine();
+            for (int j = 0; j < word.length(); j++) {
+                arr[i][j] = word.charAt(j);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < 15; j++) {
+            for (int i = 0; i < 5; i++) {
+                if (arr[i][j] == '\0') continue;
+                sb.append(arr[i][j]);
+            }
+        }
+        System.out.println(sb);
+    }
+}
