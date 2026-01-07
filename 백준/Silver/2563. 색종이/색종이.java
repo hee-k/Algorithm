e61a02d324776ba177bad28516491f2af2c2725c
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2026-01-07 21:55:39 +0900

    [Silver V] Title: 색종이, Time: 108 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[][] paper = new boolean[100][100];
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int dx = 0; dx < 10; dx++) {
+                for (int dy = 0; dy < 10; dy++) {
+                    paper[x + dx][y + dy] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (paper[i][j]) count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
