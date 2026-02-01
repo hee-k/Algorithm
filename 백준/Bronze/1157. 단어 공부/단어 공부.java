f86eb71ce05aba051f59d9d1c8b4f4e4518d79c6
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2026-02-01 14:23:41 +0900

    [Bronze I] Title: 단어 공부, Time: 236 ms, Memory: 21220 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,26 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String word = br.readLine().toUpperCase();
+        String s = br.readLine().toUpperCase();
 
         int[] count = new int[26];
 
-        for (int i = 0 ; i < word.length(); i++){
-            count[word.charAt(i) - 'A'] += 1;
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'A']++;
         }
 
         int max = 0;
-        char result = '?';
+        char result = ' ';
 
-        for (int i = 0 ; i < 26; i++){
-            if (count[i] > max){
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
                 max = count[i];
-                result = (char) ('A' + i);
-            } else if (count[i] == max){
+                result = (char)('A' + i);
+            } else if (count[i] == max) {
                 result = '?';
             }
         }
