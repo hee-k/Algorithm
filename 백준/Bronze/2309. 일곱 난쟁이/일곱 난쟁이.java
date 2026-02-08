635663f3dfa86f40e647fbacf76de2e1c097d2c4
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2026-02-08 23:51:35 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                int sum = 0;
+                for (int k = 0; k < 9; k++) {
+                    if (k != i && k != j) {
+                        sum += arr[k];
+                    }
+                }
+                if (sum == 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    Arrays.sort(arr);
+                    for (int k = 2; k < 9; k++) {
+                        sb.append(arr[k]).append("\n");
+                    }
+                    System.out.println(sb);
+                    return;
+                }
+            }
+        }
+    }
+}
