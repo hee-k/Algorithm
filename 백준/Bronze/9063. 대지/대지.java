e6d43d6cb9e8474ccbc0415e1e0c015f0f28ec12
Heesun Kim <76414250+hee-k@users.noreply.github.com>
2026-02-18 23:46:19 +0900

    [Bronze III] Title: 대지, Time: 252 ms, Memory: 27556 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int minX = 10000;
+        int maxX = -10000;
+        int minY = 10000;
+        int maxY = -10000;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(minX, x);
+            maxX = Math.max(maxX, x);
+            minY = Math.min(minY, y);
+            maxY = Math.max(maxY, y);
+        }
+
+        System.out.println((maxX - minX) * (maxY - minY));
+    }
+}
